30f16c432878974e666100f3a6ff5934b499933c
angellee177 <angelriapurnamasari17@gmail.com>
2019-08-27 09:02:19 +0700

    [angel] setting Gitlab CI/CD

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv/config');
-const port = process.env.PORT || 5002;
+const port = process.env.PORT;
 const mongoose = require('mongoose');
 const dbConnection ={
     development: "mongodb://localhost/todo", 
